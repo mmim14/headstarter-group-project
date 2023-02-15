3a44e7e2f303787db10e26cd711ea17ea1d36f38
IbrahimAli1219 <110525821+IbrahimAli1219@users.noreply.github.com>
2023-02-14 20:11:33 -0500

    auth

--- headstarter/src/App.js
+++ headstarter/src/App.js
@@ -1,9 +1,16 @@
 
+import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import AuthDetails from './components/AuthDetails';
 
 function App() {
   return (
     <div className="App">
+      <SignIn />
+      <SignUp />
+      <AuthDetails />
     </div>
   );
 }
